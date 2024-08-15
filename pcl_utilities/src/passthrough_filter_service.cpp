525516e523ba80035b64b7fc37668353db1c5d9b
Tags01 <christiantag2012@gmail.com>
2024-08-15 14:23:23 -0400

    Builds, Lints, Visual test passes. See notes: Converted pcl_passthrough_filter from ROS1 -> ROS2 Tested in Intel 2024, Clang++-19, G++ 4.8
    passthrough_filter:
    - introduced class format, moved main functionality to the constructor, and marked data private.
    - added missing header files
    - added error checking for all input parameters: all parameters must produce a volumn with a
      positive, non-zero, non-NaN size. Node will shutdown on error. This was moved to its own
      function.
    - Convert `fromROSMsg` -> `moveFromROSMsg`.
    - preserve TF frame information in the result.
    - explicit shared ptr definition with `std::make_shared`.
    - convert parameter namespace to `std::string_view` to follow google style guidelines.
    
    simple_test:
     - omit testing functionality, this is harder to test but is easy to verify visually.
       These tests will be replaced anyways.
    
    CMake:
     - Added executables
     - Added notes about why the program must not use `-Ofast` or `-ffast-math`
    
    Added appropriate launch files for the node and testing (includes comments)

--- pcl_utilities/src/passthrough_filter_service.cpp
+++ pcl_utilities/src/passthrough_filter_service.cpp
@@ -1,22 +1,41 @@
+/*
+ * Author: Brian Flynn
+ * Date: Nov 17, 2022
+ * Editors: Christian Tagliamonte
+ * Last Modified: Aug 15, 2024
+ * Adapted from:
+ * https://github.com/uml-robotics/armada_behaviors/commits/main/armada_flexbe_utilities/src/service/pcl_passthrough_filter_service.cpp
+ *
+ * Description: Starts up a service for filtering point cloud data with a bounding box.
+ *
+ * Input: sensor_msgs/PointCloud2
+ * Output: sensor_msgs/PointCloud2
+ *
+ * Usage:
+ *    `ros2 launch pcl_utilities passthrough_filter.xml`
+ */
 #include <functional>  // std::bind
 #include <memory>  // std::make_shared
+#include <sstream>  // std::stringstream
 #include <string_view>  // std::string_view
 #include <utility>  // std::move
 
-#include "rclcpp/node.hpp"
-#include "rclcpp/service.hpp"
-#include "armada_utility_msgs/srv/pcl_passthrough_filter.hpp"
-#include "sensor_msgs/msg/point_cloud2.hpp"
-
 #include "pcl/impl/point_types.hpp"
-#include "pcl/point_cloud2.h"
+#include "pcl/point_cloud.h"
 #include "pcl/filters/passthrough.h"
 #include "pcl_conversions/pcl_conversions.h"
 
-using armada_utility_msgs::srv::PCLPassthroughFilter;
+#include "rclcpp/node.hpp"
+#include "rclcpp/service.hpp"
+#include "sensor_msgs/msg/point_cloud2.hpp"
+
+#include "pcl_utility_msgs/srv/pcl_passthrough_filter.hpp"
+
+using pcl_utility_msgs::srv::PCLPassthroughFilter;
 constexpr std::string_view g_PARAM_NAMESPACE = "filters.passthrough.";
 
-class PassthroughFilterService : public rclcpp::Node {
+class PassthroughFilterService : public rclcpp::Node
+{
 private:
   rclcpp::Service<PCLPassthroughFilter>::SharedPtr passthrough_filter_service_;
 
@@ -36,20 +55,24 @@ public:
   PassthroughFilterService()
   : rclcpp::Node("passthrough_filter_service")
   {
-    declare_parameter<float>(g_PARAM_NAMESPACE + "x.min");
-    declare_parameter<float>(g_PARAM_NAMESPACE + "x.max");
-    declare_parameter<float>(g_PARAM_NAMESPACE + "y.min");
-    declare_parameter<float>(g_PARAM_NAMESPACE + "y.max");
-    declare_parameter<float>(g_PARAM_NAMESPACE + "z.min");
-    declare_parameter<float>(g_PARAM_NAMESPACE + "z.max");
+    std::string param_namespace {g_PARAM_NAMESPACE};
 
-    auto callback =
-        std::bind(&PassthroughFilterService::passthrough_filter, this,
-                  std::placeholders::_1, std::placeholders::_2);
+    x_min_ = declare_parameter<float>(param_namespace + "x.min");
+    x_max_ = declare_parameter<float>(param_namespace + "x.max");
+    y_min_ = declare_parameter<float>(param_namespace + "y.min");
+    y_max_ = declare_parameter<float>(param_namespace + "y.max");
+    z_min_ = declare_parameter<float>(param_namespace + "z.min");
+    z_max_ = declare_parameter<float>(param_namespace + "z.max");
 
-    passthrough_filter_service_ =
-      create_service<PCLPassthroughFilter>
-      ("passthrough_filter", std::move(callback));
+    // do not shutdown so all memory is cleaned up before exit
+    check_for_valid_limits();
+
+    auto callback = std::bind(
+      &PassthroughFilterService::passthrough_filter, this,
+      std::placeholders::_1, std::placeholders::_2);
+
+    passthrough_filter_service_ = create_service<PCLPassthroughFilter>(
+      "passthrough_filter", std::move(callback));
   }
 
   /**
@@ -68,41 +91,92 @@ public:
     PCLPassthroughFilter::Request::SharedPtr req,
     PCLPassthroughFilter::Response::SharedPtr res)
   {
-    get_parameter(g_PARAM_NAMESPACE + "x.min", x_min_);
-    get_parameter(g_PARAM_NAMESPACE + "x.max", x_max_);
-    get_parameter(g_PARAM_NAMESPACE + "y.min", y_min_);
-    get_parameter(g_PARAM_NAMESPACE + "y.max", y_max_);
-    get_parameter(g_PARAM_NAMESPACE + "z.min", z_min_);
-    get_parameter(g_PARAM_NAMESPACE + "z.max", z_max_);
+    // Retrieve and validate all parameters
+    std::string param_namespace {g_PARAM_NAMESPACE};
 
-    pcl::PointCloud<pcl::PointXYZRGB> temp_cloud;
-    moveFromROSMsg(req->cloud_in, temp_cloud);
+    get_parameter(param_namespace + "x.min", x_min_);
+    get_parameter(param_namespace + "x.max", x_max_);
+    get_parameter(param_namespace + "y.min", y_min_);
+    get_parameter(param_namespace + "y.max", y_max_);
+    get_parameter(param_namespace + "z.min", z_min_);
+    get_parameter(param_namespace + "z.max", z_max_);
+
+    if (!check_for_valid_limits()) {
+      rclcpp::shutdown();
+      return false;
+    }
+
+    auto temp_cloud = std::make_shared<pcl::PointCloud<pcl::PointXYZRGB>>();
+
+    std::string frame_id = req->cloud_in.header.frame_id;
+    moveFromROSMsg(req->cloud_in, *temp_cloud);
 
     pcl::PassThrough<pcl::PointXYZRGB> pass_x;
     pass_x.setInputCloud(temp_cloud);
     pass_x.setFilterFieldName("x");
     pass_x.setFilterLimits(x_min_, x_max_);
     // pass_x.setFilterLimitsNegative(false);
-    pass_x.filter(temp_cloud);
+    pass_x.filter(*temp_cloud);
 
     pcl::PassThrough<pcl::PointXYZRGB> pass_y;
     pass_y.setInputCloud(temp_cloud);
     pass_y.setFilterFieldName("y");
     pass_y.setFilterLimits(y_min_, y_max_);
-    pass_y.filter(temp_cloud);
+    pass_y.filter(*temp_cloud);
 
     pcl::PassThrough<pcl::PointXYZRGB> pass_z;
     pass_z.setInputCloud(temp_cloud);
     pass_z.setFilterFieldName("z");
     pass_z.setFilterLimits(z_min_, z_max_);
-    pass_z.filter(temp_cloud);
+    pass_z.filter(*temp_cloud);
 
-    toROSMsg(temp_cloud, res->cloud_out);
+    // preseve the TF frame in the output
+    toROSMsg(*temp_cloud, res->cloud_out);
+    res->cloud_out.header.frame_id = std::move(frame_id);
+
     return true;
+  }
+
+  /**
+   * Checks whether the dimensions provided as parameters are valid,
+   * If not, then then this message prints an error message and returns
+   * false.
+   *
+   * Note: This function must be called after all the values are initialized.
+   *
+   * Side-effects: this program uses the state of `this` and prints to std::stderr.
+   * @returns
+   *  a boolean indicating whether the variables are valid.
+   *
+   */
+  bool check_for_valid_limits() const
+  {
+    // Checks to make sure all inputs have a positive size
+    // Must use `!` instead since all comparisons over NaN are
+    // implitely false, so this condition catches this case.
+    if (
+      x_max_ - x_min_ > 0.f &&
+      y_max_ - y_min_ > 0.f &&
+      z_max_ - z_min_ > 0.f)
+    {
+      return true;
+    }
+
+    std::stringstream message;
+    message << "Passthrough filter bounds do not produce a positive, "
+            << "non-zero, non-NaN size:\n"
+            << "     X Dimension: " << (x_max_ - y_min_) << "\n"
+            << "     Y Dimension: " << (y_max_ - y_min_) << "\n"
+            << "     Z Dimension: " << (z_max_ - z_min_) << "\n";
+
+    RCLCPP_FATAL_STREAM(get_logger(), message.str());
+
+    return false;
   }
 };
 
-int main(int argc, char ** argv) {
+int main(int argc, char ** argv)
+{
   rclcpp::init(argc, argv);
   auto node = std::make_shared<PassthroughFilterService>();
   rclcpp::spin(node);
---
+++ pcl_utilities/src/tests/simple_test_passthrough_filter.cpp
@@ -0,0 +1,125 @@
+/*
+ * Author: Christian Tagliamonte
+ * Date: Aug 15, 2024
+ * Editors: N/A
+ * Last Modified: Aug 15, 2024
+ *
+ * Description: A node to test the passthrough_filter_service.
+ * This node listens to point cloud data from a camera
+ *   messages from the topic specified by the parameter, `point_cloud_topic.`
+ * The point cloud is passed to the service, where all points outside of a bounding box
+ * are removed. The bounds for the passthrough filter are controlled via ros parameters,
+ * these parameters default to the shape of 1x1x1 meter unit-cube centered at (0, 0, 0).
+ *
+ * NOTES:
+ * - That all of these parameters must produce a volume with a positive, non-zero size.
+ * - None of the paramters can be NaN.
+ * The node will crash if invalid input is passed to these parameters.
+ *
+ * The filtered point cloud is then published to the topic
+ *   `test_passthrough_filter_service/passthrough_filtered`
+ *
+ * Usage:
+ *    `ros2 launch pcl_utilities test_passthrough_filter_service.xml point_cloud_topic:=<POINT_CLOUD_TOPIC>`
+ */
+#include <chrono>   // std::chrono::seconds
+#include <cstddef>  // size_t
+#include <cstdint>  // int64_t
+#include <functional>  // std::bind, std::placeholders
+#include <ios>  // std::fixed, std::setprecision
+#include <memory>  // std::make_shared
+#include <sstream>  // std::stringstream
+#include <string>
+#include <utility>  // std::move
+
+#include "rclcpp/executors.hpp"
+#include "rclcpp/logger.hpp"
+#include "rclcpp/logging.hpp"
+#include "rclcpp/node.hpp"
+#include "rclcpp/publisher.hpp"
+#include "rclcpp/subscription.hpp"
+#include "rclcpp/wait_for_message.hpp"
+
+#include "sensor_msgs/msg/point_cloud2.hpp"
+
+#include "pcl_utility_msgs/srv/pcl_passthrough_filter.hpp"
+
+using pcl_utility_msgs::srv::PCLPassthroughFilter;
+using sensor_msgs::msg::PointCloud2;
+
+constexpr std::chrono::seconds MAX_WAIT_TIME {1U};
+
+class TestPassthroughFilterNode : public rclcpp::Node
+{
+private:
+  rclcpp::Client<PCLPassthroughFilter>::SharedPtr
+    passthrough_filter_client_;
+  rclcpp::Publisher<PointCloud2>::SharedPtr output_publisher_;
+  std::string camera_topic_;
+
+public:
+  TestPassthroughFilterNode()
+  : rclcpp::Node("simple_test_passthrough_filter")
+  {
+    std::string client_topic = declare_parameter<std::string>("node_client_name");
+    camera_topic_ = declare_parameter<std::string>("point_cloud_topic");
+
+    passthrough_filter_client_ =
+      create_client<PCLPassthroughFilter>(client_topic);
+
+    output_publisher_ = create_publisher<PointCloud2>(
+      "test_passthrough_filter_service/cloud_filtered", 1);
+  }
+
+  void spin()
+  {
+    while (rclcpp::ok()) {
+      PointCloud2 point_cloud_message;
+
+      bool was_retrieved = rclcpp::wait_for_message(
+        point_cloud_message, shared_from_this(),
+        camera_topic_, MAX_WAIT_TIME);
+
+      if (!was_retrieved) {
+        RCLCPP_ERROR_STREAM(
+          get_logger(),
+          "A camera message could not be retrieved within 1 second.");
+        continue;
+      }
+
+      process_point_cloud(point_cloud_message);
+    }
+  }
+
+  void process_point_cloud(PointCloud2 & point_cloud)
+  {
+    auto request = std::make_shared<PCLPassthroughFilter::Request>();
+    request->cloud_in = std::move(point_cloud);
+
+    auto response_future =
+      passthrough_filter_client_->async_send_request(request);
+
+    auto response_code = rclcpp::spin_until_future_complete(
+      shared_from_this(), response_future, MAX_WAIT_TIME);
+
+    if (response_code != rclcpp::FutureReturnCode::SUCCESS) {
+      RCLCPP_ERROR_STREAM(get_logger(), "Failed to recieve a response from the service");
+      return;
+    }
+
+    PCLPassthroughFilter::Response::SharedPtr response {
+      response_future.get()};
+    output_publisher_->publish(response->cloud_out);
+  }
+};
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+
+  auto node = std::make_shared<TestPassthroughFilterNode>();
+  node->spin();
+
+  rclcpp::shutdown();
+  return 0;
+}
